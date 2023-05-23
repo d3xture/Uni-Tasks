891980ddabd2ecbd41f8c6930cb030fcc70023d3
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-05-24 00:07:07 +0500

    Update funciton prototype.cpp

--- funciton prototype.cpp
+++ funciton prototype.cpp
@@ -1,48 +1,54 @@
 #include<iostream>
 using namespace std;
-/funciton prototype
-int GetNextNo(int n) //defination
+//funciton prototype
+
+// int GetNextNo(int n) //defination
+// {
+// 	return n+1;
+// }
+// int main()
+// {
+// 	int n = 5;
+// 	int next = GetNextNo(n); //call
+// 	cout<<next;
+// }
+
+
+// int GetNextNO(int); // we must have to put semi-colon at the end.
+ 		               // declaration
+// int main()
+// {
+// 	int n = 5;
+// 	int next = GetNextNo(n); // This only tells this function is gonna come!!
+// 	cout<<next;
+// }
+// int GetNextNo(int n)
+// {
+// 	return n+1;
+// }
+
+//example
+// int main(){
+// 	for ( int i = 0;i<10;i++);
+// }
+float max(float a, float b)
 {
-	return n+1;
+	if(a<b)
+	return a;
+	else
+    {
+        return b;
+    }
+	
 }
 int main()
 {
-	int n = 5;
-	int next = GetNextNo(n); //call
-	cout<<next;
+	int x = 4, y = 5;
+	cout<< max (x,y);
+	float a  = 4.5, b = 5.5;
+	cout<<max (a,b);
 }
 
-
-int GetNextNO(int); // we must have to put semi-colon at the end.
-		   // declaration
-main(){
-	int n = 5;
-	int next = GetNextNo(n); // This only tells this function is gonna come!!
-	cout<<next;
-}
-int GetNExtNo(int n)
-{
-	return n+1;
-}
-
-//example
-int main(){
-	for ( int i = 0; i<10; i++);
-}
-//int main()
-//{
-//	int x = 4, y = 5;
-//	cout<< max (x,y);
-//	flot a  = 4.5, b = 5.5;
-//	cout<<max (a,b);
-//}
-//float max(float a. float b)
-//{
-//	if(a<b)
-//	return a;
-//	else()
-//	return b;
-//}
 //int max( int x, int y)
 //{
 //	if (x>y)
@@ -70,14 +76,14 @@ int main(){
 
 //Pasing Values to the fuction
 // "PASS BY VALUE"
-void increament(int b)
-{
-	a++;
-	cout<<a;////if we call 'a' from here it will print 6
-}
-int main(){
-	int a = 5;
-increament (a);//from main to void it passing the value 5
+// void increament(int a)
+// {
+// 	a++;
+// 	cout<<a;////if we call 'a' from here it will print 6
+// }
+// int main(){
+// 	int a = 5;
+// increament (a);//from main to void it passing the value 5
 //cout<<a;     //if we call 'a' from here it will print 5
 //cout<<b;     //scope has to be declared! or it will throw error		
-}
+// }
