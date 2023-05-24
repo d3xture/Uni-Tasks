46c0e1a52ca30b12ea309162f6cd18ab64ebb4e4
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-05-25 00:21:47 +0500

    Create array.cpp
    rough work of lecture "ARRAY"

---
+++ array.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+//program no 1:
+
+int main()
+{
+int nos = [1,2,4,6,9,7];
+cout<<"Printing all nos ";
+cout<<nos[0]<<endl;
+cout<<nos[1]<<endl;
+cout<<nos[2]<<endl;
+cout<<nos[3]<<endl;
+cout<<nos[4]<<endl;
+cout<<nos[5]<<endl;
+cout<<nos[6]<<endl;
+
+}
+
+// program no 2:
+int mai(){
+int nos=[1,2,5,6,9,7]
+cout<<"Printing all nos ";
+
+//program no 3:
+int nos[1,2,5,6,9,7];
+cout<<"Printing al nos"'
+for(int i = 0; i<6;i++){
+    cout<<nos[i];
+}
+
+//going to each numbers is called traversing
+
+int main (){
+int k = 5;
+int nos = [12,4,5,9,7]; //declare + initialize
+
+for(int i =0; i<6;i++){ //traversing + printing 
+cout<<nos[i]<<"\t"
+}
+cout<<"-------------------"<<endl;
+for(int i =0; i<6;i++){ //traversing + modifying
+nos[i]*=k;
+
+for(int i =0; i<6;i++){ //traversing + printing
+nos[i]*<<"\t";
+
+//PROGRAM NO 1 MODIFICATION
+//method no 1
+int main()
+{
+   int nos[6]; //declaration
+   nos[0] = 1;
+   nos[1] = 2;
+   nos[2] = 3;
+   nos[5] = 100;
+   nos[3] = 7;
+   nos[4] = 9; //now we have done initialization
+}
+//we can also ask this from user the upper program
+//example
+//method no 2
+cout<<"Enter 6 nos: ";
+cin>> nos[0]<<endl;
+cin>> nos[1]<<endl;
+cin>> nos[2]<<endl;
+cin>> nos[3]<<endl;
+cin>> nos[4]<<endl;
+cin>> nos[5]<<endl;// it has repitition
+
+
+//method no 3, to avoid repitition
+
+for(int i = 0; i<6; i++)
+    cin<<nos[i];
+    
+return 0;
+} 
